6e0b47bbbf27a01559dcab3c7b3b48ea2a872583
Brittney Postma <brittney.postma@hotmail.com>
2019-09-03 14:31:31 -0400

    mario initial commit

---
+++ mario.c
@@ -0,0 +1,24 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height = get_int("Please choose a number from 1 to 8:\n");
+   
+    while (height > 0 || height <= 8)
+    {
+        for (int r = 0; r <= height; r++)
+        {
+            for (int s = (height-r); s > 0; s--)
+            {
+                printf(" ");
+            }
+            for (int h = 1; h <= r; h++)
+            {
+                printf("#");
+            }
+            printf("\n");  
+        }  
+        return 0;
+    }
+}
